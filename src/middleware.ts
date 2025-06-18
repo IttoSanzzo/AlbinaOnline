f74dd82e15abc5c5b72030cf7d96619ed7f9bee9
IttoSanzzo <konohittos@gmail.com>
2025-06-18 08:26:40 -0300

    jwt middleware updated to match new cookie name

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,10 @@ import {
 	stackMiddlewares,
 	MiddlewareFactory,
 } from "@/middlewares/core/stackHandler";
-import { withJwt } from "./middlewares/withJwt";
+import { withJwtAccessToken } from "./middlewares/withJwtAccessToken";
 import { applyIfPathMatch } from "./middlewares/core/withPathFilter";
 
 const middlewares: MiddlewareFactory[] = [
-	applyIfPathMatch(["/login", "/register"], withJwt),
+	applyIfPathMatch(["/login", "/register"], withJwtAccessToken),
 ];
 export default stackMiddlewares(middlewares);
--- src/middlewares/withJwtAccessToken.ts
+++ src/middlewares/withJwtAccessToken.ts
@@ -1,9 +1,10 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 import { MiddlewareFactory } from "./core/stackHandler";
 
-export const withJwt: MiddlewareFactory = (next) => {
+export const withJwtAccessToken: MiddlewareFactory = (next) => {
 	return async (request: NextRequest, event: NextFetchEvent) => {
-		const token = request.cookies.get("jwt")?.value;
+		const token = request.cookies.get("jwt-access-token")?.value;
+		// console.log(token);
 		if (token) {
 			return NextResponse.redirect(new URL("/", request.url));
 		}
