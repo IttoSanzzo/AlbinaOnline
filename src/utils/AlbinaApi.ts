86abd22d6c0738c6892ba678d2ebfddf45169b95
IttoSanzzo <konohittos@gmail.com>
2025-11-16 02:44:09 -0300

    linting

--- src/utils/AlbinaApi.ts
+++ src/utils/AlbinaApi.ts
@@ -1,5 +1,5 @@
+// const AlbinaApiPort = process.env.NEXT_PUBLIC_ALBINA_API_PORT;
 const AlbinaApiAddress = process.env.NEXT_PUBLIC_ALBINA_API_ADDRESS;
-const AlbinaApiPort = process.env.NEXT_PUBLIC_ALBINA_API_PORT;
 const AlbinaApiSecure = process.env.NEXT_PUBLIC_ALBINA_API_SECURE === "true";
 const AlbinaApiProtocol = AlbinaApiSecure ? "https://" : "http://";
 const AlbinaApiFullAddress = `${AlbinaApiProtocol}${AlbinaApiAddress}`;
