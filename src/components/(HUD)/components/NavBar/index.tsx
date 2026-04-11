22562a0e3f3c113886f3b19c1dca3e9414405cc9
IttoSanzzo <konohittos@gmail.com>
2026-04-11 03:49:56 -0300

    NavBar now is colored when in development

--- src/components/(HUD)/components/NavBar/index.tsx
+++ src/components/(HUD)/components/NavBar/index.tsx
@@ -3,17 +3,26 @@ import { Breadcrumbs, HideSideBarButton, NavBarModules } from "./subComponents";
 import { newStyledElement } from "@setsu-tp/styled-components";
 import styles from "./styles.module.css";
 import { PageSearchButton } from "./subComponents/PageSearchButton";
+import { env } from "process";
 
 const NavBarContainer = newStyledElement.div(styles.navBarContainer);
 const NavSideContainer = newStyledElement.div(styles.navSideContainer);
 
 const ProfileButtonContainer = newStyledElement.div(
-	styles.profileButtonContainer
+	styles.profileButtonContainer,
 );
 
 export function NavBar() {
 	return (
-		<NavBarContainer>
+		<NavBarContainer
+			style={
+				env.NODE_ENV == "development"
+					? {
+							background:
+								"repeating-linear-gradient(45deg, var(--cl-yellow-600) 0px, var(--cl-yellow-600) 20px, var(--cl-gray-700) 20px, var(--cl-gray-700) 40px)",
+						}
+					: undefined
+			}>
 			<NavSideContainer>
 				<HideSideBarButton />
 				<Breadcrumbs />
