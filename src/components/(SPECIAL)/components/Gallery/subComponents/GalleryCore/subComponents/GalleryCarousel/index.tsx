defd53395242c8a2e2ce2036101102c487a25ec2
IttoSanzzo <konohittos@gmail.com>
2026-04-14 19:42:00 -0300

    Fixed GalleryCarousel... again.

--- src/components/(SPECIAL)/components/Gallery/subComponents/GalleryCore/subComponents/GalleryCarousel/index.tsx
+++ src/components/(SPECIAL)/components/Gallery/subComponents/GalleryCore/subComponents/GalleryCarousel/index.tsx
@@ -46,6 +46,8 @@ export const GalleryCarousel = React.memo(
 					<UIBasics.Carousel
 						ref={carouselRef}
 						slidesOrigin={galleryData.images.length > 1 ? "center" : "auto"}
+						minWidth={"200px"}
+						maxWidth={"200px"}
 						slideWidth={"200px"}
 						memoryId={galleryData.images.length > 1 ? url : undefined}
 						slideChilds={galleryData.images.map((imageData) => (
