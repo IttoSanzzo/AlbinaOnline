ab70a20393d479b0c3a31b5dc40db7445cfe45ac
IttoSanzzo <konohittos@gmail.com>
2026-07-16 00:28:22 -0300

    Fixed a missing interface file

---
+++ src/libs/generated/AutoMinedData/AutoMinedSpells/index.ts
@@ -0,0 +1,10 @@
+export interface AutoMinedSpellData {
+	name: string;
+	level: string;
+	castTime: string;
+	range: string;
+	components: string;
+	duration: string;
+	type: string;
+	description: string[];
+}
